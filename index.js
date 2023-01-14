db750a6dde3e76f42737767dd24c73982208dd65
Hector17vaz <lx16987@innovaccion.mx>
2023-01-14 16:05:38 -0600

    scroll finish

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ let closed = document.querySelector(".navegul");
 navToggle.addEventListener("click", () => {
     navul.classList.toggle("menu_visible");
 });
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+    reveals[i].classList.add("active");
+    } else {
+    reveals[i].classList.remove("active");
+    }
+}
+}
+
+window.addEventListener("scroll", reveal);
